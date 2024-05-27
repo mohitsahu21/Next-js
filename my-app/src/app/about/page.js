53b5b79cf5fe1a8a6b164fb6bb849d69ef92a3b7
Mohit Sahu <125088141+mohitsahu21@users.noreply.github.com>
2024-05-28 00:53:02 +0530

    next  image

--- my-app/src/app/about/page.js
+++ my-app/src/app/about/page.js
@@ -1,9 +1,19 @@
+import Image from "next/image";
+import Background from '../../../public/Background.jpg'
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight : '100',
+  subsets : ['latin'],
+  display :'swap'
+})
 
 const About = () =>{
     return(
-        <h1>
+        <h1 className={roboto.className}>
             About us
-            
+            <Image src={Background}
+            width={200} height={200}/>
         </h1>
     )
 }
